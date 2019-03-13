7fd5ccd4bce3c91cae957d95cf9e9467082f8e3f
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-13 14:16:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,11 +2,27 @@ class Book
 
 def initialize (title)
   @title = title
+
 end 
 
 def title
   @title
 end 
 
+def author
+  @author
+end 
+
+def author= (new_author)
+@author = new_author
+end 
+
+def page_count 
+  @page_count
+end 
+
+def page_count= (num)
+  @page_count = num
+end 
 end
 
