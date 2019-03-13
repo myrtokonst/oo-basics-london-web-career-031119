a4704f264287129b75aabe4f54db4481e1eb5e3a
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-13 14:21:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,33 +5,8 @@ def initialize (title)
 
 end 
 
-def title
-  @title
-end 
-
-def author
-  @author
-end 
-
-def author= (new_author)
-@author = new_author
-end 
-
-def page_count 
-  @page_count
-end 
-
-def page_count= (num)
-  @page_count = num
-end 
-
-def genre=(genre)
-  @genre = genre
-end 
-
-def genre
-  @genre 
-end 
+attr_accessor :author, :page_count, :genre 
+attr_reader :title
 
 def turn_page
   puts "Flipping the page...wow, you read fast!"
